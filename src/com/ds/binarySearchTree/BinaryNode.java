fba7fd44f8e3fccf9151f9d63d9f073fbd354fc8
sheetal-10 <sheetalp257@gmail.com>
2021-08-25 23:43:36 +0530

    Added: code for creating a binary tree

---
+++ src/com/ds/binarySearchTree/BinaryNode.java
@@ -0,0 +1,24 @@
+package com.ds.binarySearchTree;
+
+/**
+ * Ability to create Node for Binary Search Tree
+ * @author sheetal
+ * @since 2021-08-25
+ */
+public class BinaryNode<K extends Comparable<K>> {
+	 K key;
+	    BinaryNode<K> left;
+	    BinaryNode<K> right;
+
+	    public BinaryNode(K key)
+	    {
+	        super();
+	        this.key = key;
+	        this.left = null;
+	        this.right = null;
+	    }
+	    @Override
+	    public String toString() {
+	        return "BinaryNode{" + "key=" + key + ", left=" + left + ", right=" + right + '}';
+	    }
+}
--- src/com/ds/binarySearchTree/BinarySearchTree.java
+++ src/com/ds/binarySearchTree/BinarySearchTree.java
@@ -3,10 +3,50 @@ package com.ds.binarySearchTree;
 /**
  * Method of creation of Binary Tree 
  * Methods of operations which can perform on Binary tree
- * @author Sheetal
+ * @author sheetal
  * @since 2021-08-25
  */
-public class BinarySearchTree {
+public class BinarySearchTree<K extends Comparable<K>> {
 	public static void main(String[] args) {
-	System.out.println("Welcome to binary search tree");}
+        System.out.println("Welcome to BinarySearchTree program");
+        BinarySearchTree<Integer> binaryTree = new BinarySearchTree<>();
+        binaryTree.add(56);
+        binaryTree.add(30);
+        binaryTree.add(70);
+        System.out.println(binaryTree);
+    }
+    private BinaryNode<K> root;
+    public void add(K key)
+    {
+        this.root = this.insertData(root,key);
+    }
+
+    /**
+     * Ability to insert Node in Binary Search Tree
+     * @param current
+     * @param key
+     * @return current
+     */
+    public BinaryNode<K> insertData(BinaryNode<K> current, K key)
+    {
+        if(current ==  null)
+            return new BinaryNode<>(key);
+        int compareResult = key.compareTo(current.key);
+        if(compareResult==0)
+            return current;
+        if(compareResult<0)
+        {
+            current.left = insertData(current.left, key);
+        }
+        else
+        {
+            current.right = insertData(current.right, key);
+        }
+        return current;
+    }
+    @Override
+    public String toString()
+    {
+        return "MyBinary [root=" + root + "]";
+    }
 }
