7e4f03582b28a3bd07376e60cf43c73f2bb29a28
sheetal-10 <sheetalp257@gmail.com>
2021-08-25 23:14:57 +0530

    Added: welcome message

---
+++ src/com/ds/binarySearchTree/BinarySearchTree.java
@@ -0,0 +1,12 @@
+package com.ds.binarySearchTree;
+
+/**
+ * Method of creation of Binary Tree 
+ * Methods of operations which can perform on Binary tree
+ * @author Sheetal
+ * @since 2021-08-25
+ */
+public class BinarySearchTree {
+	public static void main(String[] args) {
+	System.out.println("Welcome to binary search tree");}
+}
