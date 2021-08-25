1b7fba94bd162aa7020f84085b07b5f7eb3c5b2f
sheetal-10 <sheetalp257@gmail.com>
2021-08-25 23:58:41 +0530

    Added : code for creating size of binary tree

--- src/com/ds/binarySearchTree/BinaryNode.java
+++ src/com/ds/binarySearchTree/BinaryNode.java
@@ -6,19 +6,19 @@ package com.ds.binarySearchTree;
  * @since 2021-08-25
  */
 public class BinaryNode<K extends Comparable<K>> {
-	 K key;
-	    BinaryNode<K> left;
-	    BinaryNode<K> right;
+	K key;
+	BinaryNode<K> left;
+	BinaryNode<K> right;
 
-	    public BinaryNode(K key)
-	    {
-	        super();
-	        this.key = key;
-	        this.left = null;
-	        this.right = null;
-	    }
-	    @Override
-	    public String toString() {
-	        return "BinaryNode{" + "key=" + key + ", left=" + left + ", right=" + right + '}';
-	    }
+	public BinaryNode(K key) {
+		super();
+		this.key = key;
+		this.left = null;
+		this.right = null;
+	}
+
+	@Override
+	public String toString() {
+		return "BinaryNode{" + "key=" + key + ", left=" + left + ", right=" + right + '}';
+	}
 }
--- src/com/ds/binarySearchTree/BinarySearchTree.java
+++ src/com/ds/binarySearchTree/BinarySearchTree.java
@@ -8,12 +8,24 @@ package com.ds.binarySearchTree;
  */
 public class BinarySearchTree<K extends Comparable<K>> {
 	public static void main(String[] args) {
-        System.out.println("Welcome to BinarySearchTree program");
+		System.out.println("Welcome to BinarySearchTree program");
         BinarySearchTree<Integer> binaryTree = new BinarySearchTree<>();
         binaryTree.add(56);
         binaryTree.add(30);
         binaryTree.add(70);
+        binaryTree.add(22);
+        binaryTree.add(40);
+        binaryTree.add(60);
+        binaryTree.add(95);
+        binaryTree.add(11);
+        binaryTree.add(65);
+        binaryTree.add(3);
+        binaryTree.add(16);
+        binaryTree.add(63);
+        binaryTree.add(67);
+        int size = binaryTree.getSize();
         System.out.println(binaryTree);
+        System.out.println("size of binary tree:"+size);
     }
     private BinaryNode<K> root;
     public void add(K key)
@@ -43,6 +55,16 @@ public class BinarySearchTree<K extends Comparable<K>> {
             current.right = insertData(current.right, key);
         }
         return current;
+    }
+    
+    public int getSize()
+    {
+        return this.getSizeBinary(root);
+    }
+
+    public int getSizeBinary(BinaryNode<K> current)
+    {
+        return current == null ? 0 : 1 + this.getSizeBinary(current.left)+this.getSizeBinary(current.right);
     }
     @Override
     public String toString()
